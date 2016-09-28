9803dbf536f411c7c70f0a343327b5cf4ef44646
Dmitry Kolinchuk <dmko@sitecore.net>
2016-09-28 17:03:52 +0300

    Prevents too many redirects when accessing an item with MVC layout using Experience Editor after session expiration

---
+++ src/Sitecore.Support.126992/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.126992")]
+[assembly: AssemblyProduct("Sitecore.Support.126992")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescriptionAttribute("Prevents too many redirects when accessing an item with MVC layout using Experience Editor after session expiration")]
